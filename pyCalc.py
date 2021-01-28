5f018a98a4739260412de3a45db39f5432e46ed3
asifmostofa18 <asifmostofa18@gmail.com>
2021-01-28 19:33:00 +0600

    Second Commit

--- pyCalc.py
+++ pyCalc.py
@@ -8,10 +8,14 @@ op = input()
 
 def add(x, y):
     return x + y
+def sub(x,y):
+    return x - y
 
 if op == "+":
   print("Result is ", add(first_num, second_num))
 
+elif op == "-":
+  print("Result is ", sub(first_num, second_num))
 
 
 
